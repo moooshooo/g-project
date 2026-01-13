18bbc16bbc3ce677ca2008154207b58d23dcdec5
Mosho <moises.leon@chasacademy.se>
2026-01-13 11:04:05 +0100

    New approach to the film projekt

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <a href="https://www.toaddrive.app" target="_blank">
+          Extern sida
+        </a>
+        <p>Copy it's your right! 2026</p>
+      </footer>
+  )
+}
+
+export default Footer
